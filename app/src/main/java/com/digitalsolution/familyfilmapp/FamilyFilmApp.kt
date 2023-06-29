ebe949188214985812de35593a4cf455ff017b9b
Pablo Hurtado Gonzalo <79223844+Coshiloco@users.noreply.github.com>
2023-06-30 01:59:49 +0200

    FFA-3 Setup Hilt Inyecction Dependencies (#6)
    * Add Hilt Dependencies Reviewed
    
    * Improve Code Format

---
+++ app/src/main/java/com/digitalsolution/familyfilmapp/FamilyFilmApp.kt
@@ -0,0 +1,7 @@
+package com.digitalsolution.familyfilmapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FamilyFilmApp: Application()
--- app/src/main/java/com/digitalsolution/familyfilmapp/MainActivity.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.digitalsolution.familyfilmapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,7 +13,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.digitalsolution.familyfilmapp.navigation.AppNavigation
 import com.digitalsolution.familyfilmapp.ui.theme.FamilyFilmAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

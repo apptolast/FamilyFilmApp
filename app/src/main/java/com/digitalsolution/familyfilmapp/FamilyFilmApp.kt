77b726149ef0399b8b51f0cd1ffcbcb1e42f602c
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2023-06-30 01:18:58 +0200

    Add Hilt Dependencies Reviewed

--- app/src/main/java/com/digitalsolution/familyfilmapp/FamilyFilmApp.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/FamilyFilmApp.kt
@@ -3,7 +3,5 @@ package com.digitalsolution.familyfilmapp
 import android.app.Application
 import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
-
-
 @HiltAndroidApp
 class FamilyFilmApp: Application() {}
--- app/src/main/java/com/digitalsolution/familyfilmapp/di/ApplicationModule.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/di/ApplicationModule.kt
@@ -3,7 +3,6 @@ package com.digitalsolution.familyfilmapp.di
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-
 @Module
 @InstallIn(SingletonComponent::class)
 object ApplicationModule {}

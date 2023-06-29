54eacce5498aca2c45f9b3ad1b572137b0a87aca
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2023-06-30 01:52:32 +0200

    Improve Code Format

--- app/src/main/java/com/digitalsolution/familyfilmapp/FamilyFilmApp.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/FamilyFilmApp.kt
@@ -1,7 +1,7 @@
 package com.digitalsolution.familyfilmapp
 
 import android.app.Application
-import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
+
 @HiltAndroidApp
-class FamilyFilmApp: Application() {}
+class FamilyFilmApp: Application()
--- app/src/main/java/com/digitalsolution/familyfilmapp/di/ApplicationModule.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/di/ApplicationModule.kt
@@ -3,6 +3,7 @@ package com.digitalsolution.familyfilmapp.di
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+
 @Module
 @InstallIn(SingletonComponent::class)
-object ApplicationModule {}
+object ApplicationModule

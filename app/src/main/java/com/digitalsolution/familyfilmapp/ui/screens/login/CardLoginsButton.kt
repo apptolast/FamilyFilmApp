f18e81530ba646d97bf924bd631dd026decf5225
rndevelo <rndeveloper11501@gmail.com>
2023-07-12 01:57:07 +0200

    FFA-23_Refactor login components in diferents files

---
+++ app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/login/CardLoginsButton.kt
@@ -0,0 +1,46 @@
+package com.digitalsolution.familyfilmapp.ui.screens.login
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun CardLoginsButton(
+    text: String,
+    backgroundColor: Color,
+    paddingVertical: Dp,
+//    showSnackBar: () -> Job,
+    textColor: Color = Color.Unspecified,
+    contentImage: @Composable () -> Unit = {}
+) {
+    Card(
+        onClick = { },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = paddingVertical)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(backgroundColor)
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            contentImage()
+            Text(text = text, color = textColor)
+        }
+    }
+}
--- app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/login/LoginScreen.kt
@@ -2,19 +2,13 @@ package com.digitalsolution.familyfilmapp.ui.screens.login
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -28,13 +22,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.digitalsolution.familyfilmapp.R
-import com.digitalsolution.familyfilmapp.getGoogleFontFamily
+import com.digitalsolution.familyfilmapp.ui.theme.FamilyFilmAppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -106,99 +97,18 @@ fun LoginContent(
     }
 }
 
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-private fun CardLoginMain(
-    textFieldEmailState: String,
-    changeEmailState: (String) -> Unit,
-    textPasswordState: String,
-    changePasswordState: (String) -> Unit
-) {
-    Card {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(14.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(R.drawable.logo_film_family),
-                contentDescription = "Snail Logo",
-                modifier = Modifier
-                    .width(134.dp)
-                    .padding(8.dp)
-            )
-            Text(
-                text = "Film Family",
-                color = MaterialTheme.colorScheme.background,
-                textAlign = TextAlign.Center,
-                fontFamily = "Alfa Slab One".getGoogleFontFamily(),
-                fontSize = 36.sp
-            )
-            Text(
-                text = "Shared your films with your family and friends",
-                fontFamily = "Anton".getGoogleFontFamily(),
-                textAlign = TextAlign.Center,
-                fontSize = 14.sp
-            )
-            OutlinedTextField(
-                value = textFieldEmailState,
-                onValueChange = changeEmailState,
-                modifier = Modifier.padding(vertical = 4.dp),
-                label = { Text(text = "Enter your email", color = Color.Gray) }
-            )
-            OutlinedTextField(
-                value = textPasswordState,
-                onValueChange = changePasswordState,
-                modifier = Modifier.padding(vertical = 4.dp),
-                label = { Text(text = "Enter your password", color = Color.Gray) }
-            )
-        }
-    }
-}
-
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-private fun CardLoginsButton(
-    text: String,
-    backgroundColor: Color,
-    paddingVertical: Dp,
-//    showSnackBar: () -> Job,
-    textColor: Color = Color.Unspecified,
-    contentImage: @Composable () -> Unit = {}
-) {
-    Card(
-        onClick = { },
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = paddingVertical)
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(backgroundColor)
-                .padding(10.dp),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            contentImage()
-            Text(text = text, color = textColor)
-        }
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
 fun LoginScreenPreview() {
-//    FamilyFilmAppTheme {
-//        LoginContent(
-//            textFieldEmailState = "",
-//            textPasswordState = "",
-//            changeEmailState = {},
-//            changePasswordState = {},
-//            showSnackBar = {}
-//        )
-//    }
+    FamilyFilmAppTheme {
+        LoginContent(
+            innerPadding = PaddingValues(),
+            textFieldEmailState = "",
+            textPasswordState = "",
+            changeEmailState = {},
+            changePasswordState = {}
+        )
+    }
 }
 
 

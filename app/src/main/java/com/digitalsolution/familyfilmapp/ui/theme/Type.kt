d403f8e605985fd9ff1d823a848e03dca2890f06
Gonzalo Renedo <108679975+rndevelo@users.noreply.github.com>
2023-07-18 02:38:39 +0200

    FFA-26_defined Typography fonts (#19)

--- app/src/main/java/com/digitalsolution/familyfilmapp/ui/theme/Type.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/ui/theme/Type.kt
@@ -3,11 +3,12 @@ package com.digitalsolution.familyfilmapp.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
-import com.digitalsolution.familyfilmapp.getGoogleFontFamily
+import com.digitalsolution.familyfilmapp.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -18,10 +19,9 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
-
     titleLarge = TextStyle(
         fontSize = 26.sp,
-        fontFamily = "Holtwood One SC".getGoogleFontFamily(),
+        fontFamily = FontFamily(Font(R.font.holtwood_one_sc)),
         fontWeight = FontWeight(200),
         color = Color(0xFFFFFFFF),
         textAlign = TextAlign.Center
@@ -29,26 +29,9 @@ val Typography = Typography(
 
     titleMedium = TextStyle(
         fontSize = 18.sp,
-        fontFamily = "Homenaje".getGoogleFontFamily(),
+        fontFamily = FontFamily(Font(R.font.homenaje)),
         fontWeight = FontWeight(400),
         color = Color(0xFFFFFFFF),
         textAlign = TextAlign.Center
     )
-
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )

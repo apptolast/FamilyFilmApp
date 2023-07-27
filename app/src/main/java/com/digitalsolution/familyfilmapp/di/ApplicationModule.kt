be456569bab01c9b5a2dc1889e78ea540e026125
Alberto Hidalgo <hgarcia.alberto@gmail.com>
2023-07-27 20:40:10 +0200

    FFA-4 Setup Firebase (#23)

--- app/src/main/java/com/digitalsolution/familyfilmapp/di/ApplicationModule.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/di/ApplicationModule.kt
@@ -1,9 +1,25 @@
 package com.digitalsolution.familyfilmapp.di
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object ApplicationModule
+object ApplicationModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebaseFirestore(): FirebaseFirestore = Firebase.firestore
+
+    @Singleton
+    @Provides
+    fun provideFirebaseAuth(): FirebaseAuth = Firebase.auth
+}

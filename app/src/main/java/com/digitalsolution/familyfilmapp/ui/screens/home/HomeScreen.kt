87eaeda58a86c37039f1f5342ef0f0d8cf500d24
Alberto Hidalgo <hgarcia.alberto@gmail.com>
2023-09-09 00:59:20 +0100

    FFA-27 WIP - Logout problem

--- app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/home/HomeScreen.kt
@@ -11,15 +11,13 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.digitalsolution.familyfilmapp.navigation.Routes
-import com.digitalsolution.familyfilmapp.popUpToNavigate
 import com.digitalsolution.familyfilmapp.ui.components.TopBar
+import com.digitalsolution.familyfilmapp.ui.screens.login.LoginViewModel
 import com.digitalsolution.familyfilmapp.ui.theme.FamilyFilmAppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -27,19 +25,20 @@ import com.digitalsolution.familyfilmapp.ui.theme.FamilyFilmAppTheme
 fun HomeScreen(
     navController: NavController,
     viewModel: HomeViewModel = hiltViewModel(),
+    loginViewModel: LoginViewModel = hiltViewModel()
 ) {
-    if (viewModel.state.value) {
-        LaunchedEffect(key1 = true) {
-            navController.popUpToNavigate(Routes.Login.routes, Routes.Home.routes)
-        }
-    }
-
     Scaffold(
-        topBar = { TopBar() }
+        topBar = {
+            TopBar()
+        }
     ) { paddingValues ->
         HomeContent(
             modifier = Modifier.padding(paddingValues),
-            onClickLogout = { viewModel.logout() }
+            logout = {
+                viewModel.logout()
+                loginViewModel.updateLoginState()
+                navController.navigateUp()
+            }
         )
     }
 }
@@ -47,10 +46,10 @@ fun HomeScreen(
 @Composable
 fun HomeContent(
     modifier: Modifier = Modifier,
-    onClickLogout: () -> Unit
+    logout: () -> Unit
 ) {
     Column(modifier = modifier.fillMaxSize()) {
-        Button(onClick = onClickLogout) {
+        Button(onClick = logout) {
             Text(text = "Logout")
         }
         Row(

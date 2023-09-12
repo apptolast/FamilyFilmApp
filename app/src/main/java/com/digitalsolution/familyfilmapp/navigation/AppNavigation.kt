98ed13a5b492d1cca0f17a39b12bfad764608844
rndevelo <rndeveloper11501@gmail.com>
2023-09-12 04:49:04 +0200

    FFA-25 Home Screen Lazy Column to Lazy Row

--- app/src/main/java/com/digitalsolution/familyfilmapp/navigation/AppNavigation.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/navigation/AppNavigation.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.digitalsolution.familyfilmapp.ui.screens.home.screen.HomeScreen
+import com.digitalsolution.familyfilmapp.ui.screens.home.HomeScreen
 import com.digitalsolution.familyfilmapp.ui.screens.login.LoginScreen
 import com.digitalsolution.familyfilmapp.ui.screens.splash.SplashScreen
 

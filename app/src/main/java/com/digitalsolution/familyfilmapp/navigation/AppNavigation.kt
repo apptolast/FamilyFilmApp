895333d132aa0ed279627d00b3d1d3a43ab9fa54
Coshiloco <pablohurtadohg@gmail.com>
2023-09-18 00:36:38 +0200

    FFA-41 Replace for Search FloatingActionBottom

--- app/src/main/java/com/digitalsolution/familyfilmapp/navigation/AppNavigation.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/navigation/AppNavigation.kt
@@ -1,6 +1,11 @@
 package com.digitalsolution.familyfilmapp.navigation
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -9,6 +14,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -26,6 +32,7 @@ fun AppNavigation() {
 
     val navController = rememberNavController()
     var isBottomBarVisible by rememberSaveable { mutableStateOf(false) }
+    var searchBottomVisible by rememberSaveable { mutableStateOf(false) }
 
     Scaffold(
         topBar = {
@@ -37,7 +44,21 @@ fun AppNavigation() {
             if (isBottomBarVisible) {
                 BottomBar(navController = navController)
             }
-        }
+        },
+        floatingActionButton = {
+            if (searchBottomVisible) {
+                FloatingActionButton(
+                    onClick = {}
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = null,
+                        modifier = Modifier.padding(10.dp)
+                    )
+                }
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End
     ) { paddingValues ->
         NavHost(
             navController = navController,
@@ -75,6 +96,7 @@ fun AppNavigation() {
                 false
             }
         }
+        searchBottomVisible = destination.route == Routes.Home.routes
     }
 }
 

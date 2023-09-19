79c5feb59979b56301213f96585c1536f105ae3c
Alberto Hidalgo <hgarcia.alberto@gmail.com>
2023-09-20 00:52:49 +0100

    FFA-39 UI improvements

--- app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/search/SearchScreen.kt
@@ -13,11 +13,15 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -30,17 +34,15 @@ fun SearchScreen(
     navController: NavController,
     viewModel: SearchScreenViewModel = hiltViewModel()
 ) {
-
     SearchContent(viewModel)
 }
-
 
 @Composable
 fun SearchContent(
     viewModel: SearchScreenViewModel
 ) {
-
     var searchText by rememberSaveable { mutableStateOf("") }
+    val focusRequester = remember { FocusRequester() }
 
     Column(
         modifier = Modifier
@@ -49,7 +51,9 @@ fun SearchContent(
             .padding(12.dp)
     ) {
         OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(focusRequester),
             value = searchText,
             onValueChange = { searchText = it },
             shape = RoundedCornerShape(12.dp),
@@ -59,9 +63,6 @@ fun SearchContent(
             label = {
                 Text(text = "Search film or series for add to the group are you join")
             },
-            placeholder = {
-                Text(text = "Search film or series for add to the group are you join")
-            }
         )
 
         LazyColumn {
@@ -69,6 +70,10 @@ fun SearchContent(
                 FilmItem(film = item)
             }
         }
+    }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
     }
 }
 

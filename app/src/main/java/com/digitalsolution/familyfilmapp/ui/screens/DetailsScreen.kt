9bd47097595c5c594c096f14a1b484ce2b882926
Alberto Hidalgo <hgarcia.alberto@gmail.com>
2023-09-20 02:04:02 +0100

    FFA-42 Scroll Improvements

--- app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/DetailsScreen.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/DetailsScreen.kt
@@ -9,9 +9,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Visibility
@@ -23,85 +23,88 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
+import com.digitalsolution.familyfilmapp.R
 import com.digitalsolution.familyfilmapp.ui.theme.FamilyFilmAppTheme
 
 @Composable
 fun DetailsScreen(navController: NavController) {
 
-    val scrollState = rememberScrollState()
-    val localDensity = LocalDensity.current.density
+    val lazyListState = rememberLazyListState()
+    var scrolledY = 0f
+    var previousOffset = 0
 
-    Column(
+    LazyColumn(
         modifier = Modifier.fillMaxSize(),
+        state = lazyListState
     ) {
-        AsyncImage(
-            model = "https://loremflickr.com/400/400/cat?lock=37",
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(bottomStartPercent = 5, bottomEndPercent = 5))
-                .height(200.dp - (scrollState.value.dp / localDensity))
-                .alpha(1f - (scrollState.value / localDensity) / 170f),
-            contentScale = ContentScale.Crop
-        )
-        Column(
-            modifier = Modifier.padding(18.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "Cat's Returns",
-                style = MaterialTheme.typography.headlineLarge.copy(fontWeight = FontWeight.Bold)
-            )
-            Row(
+        item {
+            AsyncImage(
+                model = "https://loremflickr.com/400/400/cat?lock=37",
+                contentDescription = null,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(vertical = 12.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(text = "2023")
-                Text(text = "1h 34min")
-                Text(text = "+18")
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 10.dp),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
-                    DetailsButtonContent(icon = Icons.Default.Add, text = "Add to see")
-                }
-                Spacer(modifier = Modifier.width(14.dp))
-                OutlinedButton(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
-                    DetailsButtonContent(icon = Icons.Default.Visibility, text = "Don't seen")
-                }
-            }
-            Text(
-                text = "Description",
-                style = MaterialTheme.typography.headlineSmall,
-                modifier = Modifier.padding(top = 15.dp)
+                    .clip(RoundedCornerShape(bottomStartPercent = 5, bottomEndPercent = 5))
+                    .height(250.dp)
+                    .graphicsLayer {
+                        scrolledY += lazyListState.firstVisibleItemScrollOffset - previousOffset
+                        translationY = scrolledY * 0.5f
+                        previousOffset = lazyListState.firstVisibleItemScrollOffset
+                    },
+                contentScale = ContentScale.Crop
             )
-            Text(
-                text = "Lorem ipsum dolor sit amet consectetur adipiscing elit, nunc nibh erat cum ultrices sociis, nisi facilisi per mollis torquent laoreet. Ultricies tellus cubilia vulputate iaculis varius, aliquam enim velit egestas. Malesuada etiam tempus lectus facilisis nam at ultricies, mus fermentum sollicitudin natoque sodales.\n" +
-                        "\n" +
-                        "Effendi facilisi integer magna porttitor rutrum sodales aliquet volutpat, porta aenean lectus nisi ridiculus penatibus leo rhoncus semper, sagittis libero dignissim pellentesque urna fames in. Natoque volutpat imperdiet dis quisque cum iaculis, senectus euismod molestie mollis cubilia purus, dictumst cras nec platea dictum. Mollis sodales mus sed ligula condimentum dui aliquet, sociis scelerisque senectus commodo convallis taciti, donec hac gravida cursus id viverra. +\n" +
-                        "\n" +
-                        "Eleifend facilisi integer magna porttitor rutrum sodales aliquet volutpat, porta aenean lectus nisi ridiculus penatibus leo rhoncus semper, sagittis libero dignissim pellentesque urna fames in. Natoque volutpat imperdiet dis quisque cum iaculis, senectus euismod molestie mollis cubilia purus, dictumst cras nec platea dictum. Mollis sodales mus sed ligula condimentum dui aliquet, sociis scelerisque senectus commodo convallis taciti, donec hac gravida cursus id viverra.",
-                modifier = Modifier
-                    .padding(vertical = 15.dp)
-                    .verticalScroll(scrollState)
-            )
+            Column(
+                modifier = Modifier.padding(18.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Cat's Returns",
+                    style = MaterialTheme.typography.headlineLarge.copy(fontWeight = FontWeight.Bold)
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 12.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = "2023")
+                    Text(text = "1h 34min")
+                    Text(text = "+18")
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 10.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                        DetailsButtonContent(icon = Icons.Default.Add, text = "Add to see")
+                    }
+                    Spacer(modifier = Modifier.width(14.dp))
+                    OutlinedButton(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                        DetailsButtonContent(icon = Icons.Default.Visibility, text = "Don't seen")
+                    }
+                }
+                Text(
+                    text = "Description",
+                    style = MaterialTheme.typography.headlineSmall,
+                    modifier = Modifier.padding(top = 15.dp)
+                )
+                Text(
+                    text = stringResource(R.string.details_screen_description_lorem_ipsum).trimIndent(),
+                    modifier = Modifier.padding(vertical = 15.dp)
+                )
+            }
         }
     }
 }
@@ -125,6 +128,6 @@ private fun DetailsButtonContent(icon: ImageVector, text: String) {
 @Composable
 fun DetailsScreenPreview() {
     FamilyFilmAppTheme {
-        DetailsScreen(navController = NavController(LocalContext.current))
+        DetailsScreen(navController = rememberNavController())
     }
 }

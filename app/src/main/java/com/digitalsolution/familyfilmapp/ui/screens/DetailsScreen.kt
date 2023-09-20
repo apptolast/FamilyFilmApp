dbd4490364669ff6949ad5e978f4e7970b6b6769
Alberto <51128924+hgarciaalberto@users.noreply.github.com>
2023-09-20 02:18:04 +0100

    FFA-42 Implement film detail screen (#57)
    * FFA-42 Implement DetailsScreen.kt
    
    * FFA-42 Implement DetailsScreen.kt
    
    * FFA-42 Scroll Improvements
    
    ---------
    
    Co-authored-by: rndevelo <rndeveloper11501@gmail.com>

--- app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/DetailsScreen.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/DetailsScreen.kt
@@ -1,20 +1,133 @@
 package com.digitalsolution.familyfilmapp.ui.screens
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
+import com.digitalsolution.familyfilmapp.R
+import com.digitalsolution.familyfilmapp.ui.theme.FamilyFilmAppTheme
 
 @Composable
 fun DetailsScreen(navController: NavController) {
 
-    Box(
+    val lazyListState = rememberLazyListState()
+    var scrolledY = 0f
+    var previousOffset = 0
+
+    LazyColumn(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        state = lazyListState
     ) {
-        Text(text = "Details Screen")
+        item {
+            AsyncImage(
+                model = "https://loremflickr.com/400/400/cat?lock=37",
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(bottomStartPercent = 5, bottomEndPercent = 5))
+                    .height(250.dp)
+                    .graphicsLayer {
+                        scrolledY += lazyListState.firstVisibleItemScrollOffset - previousOffset
+                        translationY = scrolledY * 0.5f
+                        previousOffset = lazyListState.firstVisibleItemScrollOffset
+                    },
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = Modifier.padding(18.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Cat's Returns",
+                    style = MaterialTheme.typography.headlineLarge.copy(fontWeight = FontWeight.Bold)
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 12.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = "2023")
+                    Text(text = "1h 34min")
+                    Text(text = "+18")
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 10.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Button(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                        DetailsButtonContent(icon = Icons.Default.Add, text = "Add to see")
+                    }
+                    Spacer(modifier = Modifier.width(14.dp))
+                    OutlinedButton(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                        DetailsButtonContent(icon = Icons.Default.Visibility, text = "Don't seen")
+                    }
+                }
+                Text(
+                    text = "Description",
+                    style = MaterialTheme.typography.headlineSmall,
+                    modifier = Modifier.padding(top = 15.dp)
+                )
+                Text(
+                    text = stringResource(R.string.details_screen_description_lorem_ipsum).trimIndent(),
+                    modifier = Modifier.padding(vertical = 15.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun DetailsButtonContent(icon: ImageVector, text: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(vertical = 3.dp)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            modifier = Modifier.padding(end = 6.dp)
+        )
+        Text(text = text)
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun DetailsScreenPreview() {
+    FamilyFilmAppTheme {
+        DetailsScreen(navController = rememberNavController())
     }
 }

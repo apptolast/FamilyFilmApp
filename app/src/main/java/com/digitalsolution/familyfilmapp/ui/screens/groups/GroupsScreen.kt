ce20b247f0b2308ca8c9f8d9f59f86d544de134e
Coshiloco <pablohurtadohg@gmail.com>
2023-10-11 12:46:40 +0200

    FFA-71 Fix detekt

--- app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/groups/GroupsScreen.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/groups/GroupsScreen.kt
@@ -1,28 +1,15 @@
 package com.digitalsolution.familyfilmapp.ui.screens.groups
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.NavController
-import com.digitalsolution.familyfilmapp.model.local.GroupData
 
-@Composable
-fun GroupsScreen(
-    navController: NavController,
-    viewModel: GroupViewModel = hiltViewModel()
-) {
-    val groupUiState by viewModel.state.collectAsStateWithLifecycle()
+//
+//@Composable
+//fun GroupsScreen(
+//    navController: NavController,
+//    viewModel: GroupViewModel = hiltViewModel()
+//) {
+//
+//}
 
-    GroupContent(groupUiState.groups)
-}
-
-
-@Composable
-fun GroupContent(groups: List<GroupData>) {
-
-
-}
 
 
 

70390a54ada4c2c580b6e26ed1d0f9c80c7f73eb
Alberto Hidalgo <hgarcia.alberto@gmail.com>
2023-10-13 02:05:26 +0100

    FFA-72 Rebase

--- app/src/main/java/com/digitalsolution/familyfilmapp/SharedViewModel.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/SharedViewModel.kt
@@ -1,26 +1,24 @@
 package com.digitalsolution.familyfilmapp
 
 import androidx.lifecycle.ViewModel
-import com.digitalsolution.familyfilmapp.model.local.MemeberData
-import com.digitalsolution.familyfilmapp.repositories.FakeRepositoryImpl
+import com.digitalsolution.familyfilmapp.repositories.FilmRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class SharedViewModel @Inject constructor(
-    private val fakeRepositoryImpl: FakeRepositoryImpl
+    private val filmRepository: FilmRepository
 ) : ViewModel() {
 
-    fun getGroups() = fakeRepositoryImpl.generateGroups(12)
 
-    fun getMembers() = fakeRepositoryImpl.generateMembersForGroups(12)
+    fun getMembers() = filmRepository.generateMembersForGroups(12)
 
-    fun removeElementMember(memeberData: MemeberData, memebers: List<MemeberData>) {
-        val list = memebers.toMutableList()
-        for (i in 0 until list.size) {
-            if (list[i].name.equals(memeberData.name, true)) {
-                list.remove(list[i])
-            }
-        }
-    }
+//    fun removeElementMember(memeberData: MemeberData, memebers: List<MemeberData>) {
+//        val list = memebers.toMutableList()
+//        for (i in 0 until list.size) {
+//            if (list[i].name.equals(memeberData.name, true)) {
+//                list.remove(list[i])
+//            }
+//        }
+//    }
 }
--- app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/groups/GroupsScreen.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/groups/GroupsScreen.kt
@@ -27,7 +27,6 @@ fun GroupsScreen(
     )
 }
 
-
 @Composable
 fun GroupContent(
     members: List<MemeberData>,

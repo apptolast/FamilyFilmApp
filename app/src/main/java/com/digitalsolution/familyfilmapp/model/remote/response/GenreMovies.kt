d0ed33bf56a14e41d5fcaa6f64d6c77f639eee09
Coshiloco <pablohurtadohg@gmail.com>
2023-10-22 15:22:06 +0200

    FFA-81 Add Models for the API Genres Endpoint

---
+++ app/src/main/java/com/digitalsolution/familyfilmapp/model/remote/response/GenreMovies.kt
@@ -0,0 +1,14 @@
+package com.digitalsolution.familyfilmapp.model.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class GenreMovies(
+    @SerializedName("genre_id")
+    val genreId: Int? = null,
+
+    @SerializedName("movie_id")
+    val movieId: Int? = null,
+
+    @SerializedName("movie")
+    val movie: List<MovieRemote>? = null,
+)

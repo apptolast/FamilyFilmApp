c1bd04f7b372429ee4e06a5b08c831c068c20b6c
Alberto Hidalgo <hgarcia.alberto@gmail.com>
2023-10-24 02:03:21 +0100

    FFA-81 Improve Recommendations UI

--- app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/recommend/RecommendScreen.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/ui/screens/recommend/RecommendScreen.kt
@@ -6,16 +6,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -40,10 +49,32 @@ fun RecommendScreen(
 
 @Composable
 private fun RecommendContent(movieState: MovieUiState, backendState: GenresBackendState?) {
-    val categories = movieState.categories
+    val isSelected by remember { mutableStateOf(false) }
 
     Column(modifier = Modifier.fillMaxSize()) {
-        
+        LazyVerticalGrid(columns = GridCells.Fixed(3)) {
+            items(backendState!!.genreInfo) { item ->
+
+                AssistChip(
+                    colors = AssistChipDefaults.assistChipColors(
+                        containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                    ),
+                    onClick = {
+                        isSelected != isSelected
+                    },
+                    label = {
+                        Text(
+                            item.name,
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center,
+                            overflow = TextOverflow.Ellipsis,
+                        )
+                    },
+                    modifier = Modifier.padding(horizontal = 4.dp),
+                )
+            }
+        }
+
         Text(
             text = "Comedy",
             style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.ExtraBold),

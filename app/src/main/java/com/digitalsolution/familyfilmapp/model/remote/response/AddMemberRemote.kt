5f637ad7ec78d1f19cc1237197dc42eff05ef5e0
Coshiloco <pablohurtadohg@gmail.com>
2023-12-02 12:43:13 +0100

    FFA-107 Remotes

--- app/src/main/java/com/digitalsolution/familyfilmapp/model/remote/response/AddMemberRemote.kt
+++ app/src/main/java/com/digitalsolution/familyfilmapp/model/remote/response/AddMemberRemote.kt
@@ -3,12 +3,9 @@ package com.digitalsolution.familyfilmapp.model.remote.response
 import com.google.gson.annotations.SerializedName
 
 data class AddMemberRemote(
-    @SerializedName("id")
-    val groupID: Int? = null,
-
-    @SerializedName("name")
-    val name: String? = null,
-
     @SerializedName("user_id")
     val userId: Int? = null,
+
+    @SerializedName("group_id")
+    val groupId: Int? = null,
 )

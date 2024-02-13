bc26082b159babf7e145569910d90c591be53f2d
Coshiloco <pablohurtadohg@gmail.com>
2024-02-13 02:11:02 +0100

    FFA-119 Refactor delete innecesary cards

--- app/src/main/java/com/apptolast/familyfilmapp/ui/screens/groups/components/GroupMemberCard.kt
+++ app/src/main/java/com/apptolast/familyfilmapp/ui/screens/groups/components/GroupMemberCard.kt
@@ -10,8 +10,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.RemoveCircleOutline
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -29,44 +27,31 @@ import com.apptolast.familyfilmapp.ui.theme.bold
 
 @Composable
 fun GroupMemberCard(group: Users, onRemoveMemberClick: (Int, Int) -> Unit) {
-    Card(
+    Row(
         modifier = Modifier
             .fillMaxSize()
-            .padding(8.dp),
-        shape = MaterialTheme.shapes.small,
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.scrim,
-        ),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 3.dp,
-        ),
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
-        Row(
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically,
+                .weight(1f)
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
         ) {
-            Spacer(modifier = Modifier.width(16.dp))
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxHeight(),
-                verticalArrangement = Arrangement.Center,
-            ) {
-                Text(
-                    text = group.user.email,
-                    style = MaterialTheme.typography.titleSmall.bold(),
-                    color = Color.White,
-                )
-            }
-            IconButton(onClick = { onRemoveMemberClick(group.groupID, group.userID) }) {
-                Icon(
-                    imageVector = Icons.Filled.RemoveCircleOutline,
-                    contentDescription = "Delete Member",
-                    tint = Color.White,
-                )
-            }
+            Text(
+                text = group.user.email,
+                style = MaterialTheme.typography.titleSmall.bold(),
+                color = Color.White,
+            )
+        }
+        IconButton(onClick = { onRemoveMemberClick(group.groupID, group.userID) }) {
+            Icon(
+                imageVector = Icons.Filled.RemoveCircleOutline,
+                contentDescription = "Delete Member",
+                tint = Color.White,
+            )
         }
     }
 }

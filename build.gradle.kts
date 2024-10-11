ac45dc19f76f48ac23a13e623be9c33de1714a61
hgarciaalberto <hgarcia.alberto@gmail.com>
2024-10-12 00:51:23 +0100

    Migrate to version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.7.0" apply false
-    id("com.android.library") version "8.7.0" apply false
-
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
-    id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false
-
-    id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.google.firebase.crashlytics") version "3.0.2" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
-
-    id("org.jlleitschuh.gradle.ktlint-idea") version "11.6.1" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.kotlin.serialize) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.google.dagger.hilt) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.ktlint.jlleitschuh) apply false
 }

cc6cc83e4d52df53afe065becd722855eda09b87
Alberto <51128924+hgarciaalberto@users.noreply.github.com>
2025-02-21 23:44:00 +0000

    Fix github action for deploying the app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 3
-        versionName = "0.3.0"
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true

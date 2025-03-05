30c7513f1d52de975f734461b45a54c1fe654c74
Renendo <rndeveloper11501@gmail.com>
2025-03-05 21:22:58 +0100

    FFA-126-Implement Admob and banner

--- app/src/main/java/com/apptolast/familyfilmapp/MainActivity.kt
+++ app/src/main/java/com/apptolast/familyfilmapp/MainActivity.kt
@@ -9,12 +9,14 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.apptolast.familyfilmapp.navigation.AppNavigation
 import com.apptolast.familyfilmapp.ui.theme.FamilyFilmAppTheme
+import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MobileAds.initialize(this)
         setContent {
             FamilyFilmAppTheme(dynamicColor = false) {
                 // A surface container using the 'background' color from the theme

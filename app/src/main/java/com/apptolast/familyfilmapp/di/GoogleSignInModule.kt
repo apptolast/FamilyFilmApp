0cff71e6c426de8500e3c28a9e4ba3df8ad577e3
Gonzalo Renedo <108679975+rndevelo@users.noreply.github.com>
2025-03-06 03:21:35 +0100

    FFA-151 Add the latest google sign-in feature (#317)
    Co-authored-by: hgarciaalberto <hgarcia.alberto@gmail.com>

---
+++ app/src/main/java/com/apptolast/familyfilmapp/di/GoogleSignInModule.kt
@@ -0,0 +1,35 @@
+package com.apptolast.familyfilmapp.di
+
+import android.content.Context
+import androidx.credentials.CredentialManager
+import androidx.credentials.GetCredentialRequest
+import com.apptolast.familyfilmapp.BuildConfig
+import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object GoogleSignInModule {
+
+    @Singleton
+    @Provides
+    fun signInWithGoogleOption(): GetSignInWithGoogleOption =
+        GetSignInWithGoogleOption.Builder(BuildConfig.WEB_ID_CLIENT)
+            .build()
+
+    @Singleton
+    @Provides
+    fun credentialManager(@ApplicationContext context: Context): CredentialManager = CredentialManager.create(context)
+
+    @Singleton
+    @Provides
+    fun credentialRequest(signInWithGoogleOption: GetSignInWithGoogleOption): GetCredentialRequest =
+        GetCredentialRequest.Builder()
+            .addCredentialOption(signInWithGoogleOption)
+            .build()
+}
--- app/src/main/java/com/apptolast/familyfilmapp/ui/theme/Type.kt
+++ app/src/main/java/com/apptolast/familyfilmapp/ui/theme/Type.kt
@@ -17,16 +17,16 @@ val provider = GoogleFont.Provider(
     certificates = R.array.com_google_android_gms_fonts_certs,
 )
 
-val bodyFontFamily = FontFamily(
+val titleLargeFontFamily = FontFamily(
     Font(
-        googleFont = GoogleFont("Acme"),
+        googleFont = GoogleFont("Aclonica"),
         fontProvider = provider,
     ),
 )
 
-val displayFontFamily = FontFamily(
+val titleMediumFontFamily = FontFamily(
     Font(
-        googleFont = GoogleFont("Aclonica"),
+        googleFont = GoogleFont("Acme"),
         fontProvider = provider,
     ),
 )
@@ -35,21 +35,8 @@ val displayFontFamily = FontFamily(
 val baseline = Typography()
 
 val AppTypography = Typography(
-    displayLarge = baseline.displayLarge.copy(fontFamily = displayFontFamily),
-    displayMedium = baseline.displayMedium.copy(fontFamily = displayFontFamily),
-    displaySmall = baseline.displaySmall.copy(fontFamily = displayFontFamily),
-    headlineLarge = baseline.headlineLarge.copy(fontFamily = displayFontFamily),
-    headlineMedium = baseline.headlineMedium.copy(fontFamily = displayFontFamily),
-    headlineSmall = baseline.headlineSmall.copy(fontFamily = displayFontFamily),
-    titleLarge = baseline.titleLarge.copy(fontFamily = displayFontFamily),
-    titleMedium = baseline.titleMedium.copy(fontFamily = displayFontFamily),
-    titleSmall = baseline.titleSmall.copy(fontFamily = displayFontFamily),
-    bodyLarge = baseline.bodyLarge.copy(fontFamily = bodyFontFamily),
-    bodyMedium = baseline.bodyMedium.copy(fontFamily = bodyFontFamily),
-    bodySmall = baseline.bodySmall.copy(fontFamily = bodyFontFamily),
-    labelLarge = baseline.labelLarge.copy(fontFamily = bodyFontFamily),
-    labelMedium = baseline.labelMedium.copy(fontFamily = bodyFontFamily),
-    labelSmall = baseline.labelSmall.copy(fontFamily = bodyFontFamily),
+    titleLarge = baseline.headlineLarge.copy(fontFamily = titleLargeFontFamily),
+    titleMedium = baseline.headlineMedium.copy(fontFamily = titleMediumFontFamily),
 )
 
 @Preview(showBackground = true)

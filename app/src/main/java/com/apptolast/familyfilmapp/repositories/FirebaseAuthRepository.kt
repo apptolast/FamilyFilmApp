0983d9bbe742a2a9ef5592bd41e37a36ae9c2af5
Alberto <51128924+hgarciaalberto@users.noreply.github.com>
2025-03-06 23:15:07 +0000

    FFA-162 Fix Google sign in (#320)
    Co-authored-by: Renendo <rndeveloper11501@gmail.com>

--- app/src/main/java/com/apptolast/familyfilmapp/repositories/FirebaseAuthRepository.kt
+++ app/src/main/java/com/apptolast/familyfilmapp/repositories/FirebaseAuthRepository.kt
@@ -1,16 +1,14 @@
 package com.apptolast.familyfilmapp.repositories
 
-import com.apptolast.familyfilmapp.model.local.User
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
-import java.util.Locale
-import javax.inject.Inject
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class FirebaseAuthRepositoryImpl @Inject constructor(
     private val firebaseAuth: FirebaseAuth,
@@ -86,60 +84,44 @@ class FirebaseAuthRepositoryImpl @Inject constructor(
 
     override fun logOut() = firebaseAuth.signOut()
 
-    override fun deleteAccount(email: String, password: String): Flow<Result<Boolean>> = callbackFlow {
-        val user = firebaseAuth.currentUser
-        if (user != null) {
-            val credential = EmailAuthProvider.getCredential(email, password)
-            user.reauthenticate(credential)
-            user.delete()
-                .addOnSuccessListener {
-                    launch { send(Result.success(true)) }
-                }
-                .addOnFailureListener { exception ->
-                    launch { send(Result.failure(exception)) }
-                }
-        } else {
-            launch { send(Result.failure(Exception("No user logged in"))) }
+    override fun deleteAccountWithReAuthentication(email: String, password: String): Flow<Result<Boolean>> =
+        callbackFlow {
+            val user = firebaseAuth.currentUser
+            if (user != null) {
+                val credential = EmailAuthProvider.getCredential(email, password)
+                user.reauthenticate(credential)
+                user.delete()
+                    .addOnSuccessListener {
+                        launch { send(Result.success(true)) }
+                    }
+                    .addOnFailureListener { exception ->
+                        launch { send(Result.failure(exception)) }
+                    }
+            } else {
+                launch { send(Result.failure(Exception("No user logged in"))) }
+            }
+            awaitClose()
         }
+
+    override fun deleteGoogleAccount(): Flow<Result<Boolean>> = callbackFlow {
+        firebaseAuth.currentUser?.delete()
+            ?.addOnSuccessListener {
+                launch { send(Result.success(true)) }
+            }
+            ?.addOnFailureListener { exception ->
+                launch { send(Result.failure(exception)) }
+            }
         awaitClose()
     }
 
-    override fun loginWithGoogle(idToken: String): Flow<Result<FirebaseUser>> = callbackFlow {
+    override fun loginWithGoogle(idToken: String): Flow<Result<FirebaseUser?>> = callbackFlow {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
         firebaseAuth.signInWithCredential(credential)
             .addOnSuccessListener { authResult ->
-                val user = authResult.user
-                val isNewUser = authResult.additionalUserInfo?.isNewUser
-                if (user != null) {
-                    if (isNewUser == true) {
-                        repository.createUser(
-                            User().copy(
-                                id = user.uid,
-                                email = user.email ?: "",
-                                language = Locale.getDefault().language,
-                            ),
-                            success = {
-                                launch {
-                                    send(Result.success(user))
-                                }
-                            },
-                            failure = { error ->
-                                launch {
-                                    send(Result.failure(error))
-                                }
-                            },
-                        )
-                    } else {
-                        launch {
-                            send(Result.success(user))
-                        }
-                    }
-                }
+                launch { send(Result.success(authResult.user)) }
             }
             .addOnFailureListener {
-                launch {
-                    send(Result.failure(it))
-                }
+                launch { send(Result.failure(it)) }
             }
         awaitClose()
     }
@@ -165,15 +147,37 @@ class FirebaseAuthRepositoryImpl @Inject constructor(
             }
         awaitClose()
     }
+
+    override fun getProvider(): Flow<String?> = callbackFlow {
+        val user = firebaseAuth.currentUser
+        if (user != null) {
+            // Check if the user is logged with google
+            if (user.providerData.any { it.providerId.contains(GoogleAuthProvider.GOOGLE_SIGN_IN_METHOD) }) {
+                launch { send(GoogleAuthProvider.GOOGLE_SIGN_IN_METHOD) }
+            } else if (user.providerData.any {
+                    it.providerId.contains(EmailAuthProvider.EMAIL_PASSWORD_SIGN_IN_METHOD)
+                }
+            ) {
+                launch { send(EmailAuthProvider.EMAIL_PASSWORD_SIGN_IN_METHOD) }
+            } else {
+                launch { send(null) }
+            }
+        } else {
+            launch { send(null) }
+        }
+        awaitClose()
+    }
 }
 
 interface FirebaseAuthRepository {
     fun getUser(): Flow<FirebaseUser?>
     fun login(email: String, password: String): Flow<Result<FirebaseUser?>>
     fun register(email: String, password: String): Flow<Result<FirebaseUser?>>
-    fun loginWithGoogle(idToken: String): Flow<Result<FirebaseUser>>
+    fun loginWithGoogle(idToken: String): Flow<Result<FirebaseUser?>>
     fun logOut()
-    fun deleteAccount(email: String, password: String): Flow<Result<Boolean>>
+    fun deleteAccountWithReAuthentication(email: String, password: String): Flow<Result<Boolean>>
+    fun deleteGoogleAccount(): Flow<Result<Boolean>>
     fun recoverPassword(email: String): Flow<Result<Boolean>>
     fun isTokenValid(): Flow<Boolean>
+    fun getProvider(): Flow<String?>
 }

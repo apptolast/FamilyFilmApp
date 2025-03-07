7844a9d11278381478f30bc8e79c3daf435ff2f3
hgarciaalberto <hgarcia.alberto@gmail.com>
2025-03-07 21:24:35 +0000

    FFA-164 Remove unused model files
    This commit removes several unused model files that are no longer needed in the project. These files include:
    - Genre.kt
    - AddGroupBody.kt
    - AddMemberBody.kt
    - RemoveMemberBody.kt
    - SearchMovieByNameBody.kt
    - UpdateGroupNameBody.kt
    - AddGroupRemote.kt
    - AddMemberRemote.kt
    - GenreRemote.kt
    - GroupRemote.kt
    - MovieCatalogueRemote.kt
    - MovieRemote.kt
    - UserRemote.kt
    - UserResponseRemote.kt

--- app/src/main/java/com/apptolast/familyfilmapp/model/remote/request/SearchMovieByNameBody.kt
+++
@@ -1,5 +0,0 @@
-package com.apptolast.familyfilmapp.model.remote.request
-
-import com.google.gson.annotations.SerializedName
-
-data class SearchMovieByNameBody(@SerializedName("name") val name: String)
--- app/src/main/java/com/apptolast/familyfilmapp/model/remote/request/UpdateGroupNameBody.kt
+++
@@ -1,5 +0,0 @@
-package com.apptolast.familyfilmapp.model.remote.request
-
-import com.google.gson.annotations.SerializedName
-
-data class UpdateGroupNameBody(@SerializedName("name") val name: String)

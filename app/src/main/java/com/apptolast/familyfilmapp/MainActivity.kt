a40e8c8e8cce6477b41cb80367a12b4a0f2b7cc4
hgarciaalberto <hgarcia.alberto@gmail.com>
2025-03-15 23:35:59 +0000

    FFA-126 Initialize Google Mobile Ads SDK with test device configuration

--- app/src/main/java/com/apptolast/familyfilmapp/MainActivity.kt
+++ app/src/main/java/com/apptolast/familyfilmapp/MainActivity.kt
@@ -11,10 +11,12 @@ import androidx.compose.ui.Modifier
 import com.apptolast.familyfilmapp.navigation.AppNavigation
 import com.apptolast.familyfilmapp.ui.theme.FamilyFilmAppTheme
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -22,6 +24,22 @@ class MainActivity : ComponentActivity() {
         // Fill the screen
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            // Initialize the Google Mobile Ads SDK on a background thread.
+            MobileAds.setRequestConfiguration(
+                RequestConfiguration.Builder().setTestDeviceIds(
+                    arrayListOf(
+                        // Add test devices
+                        "6EBCD242716D331EAA6673852DA6C4FA", // Androidzen
+                    ),
+                ).build(),
+            )
+            MobileAds.initialize(this@MainActivity) { initializationStatus ->
+                Timber.d("Admob initialized")
+            }
+        }
+
         setContent {
             FamilyFilmAppTheme(dynamicColor = false) {
                 // A surface container using the 'background' color from the theme
@@ -32,11 +50,6 @@ class MainActivity : ComponentActivity() {
                     AppNavigation()
                 }
             }
-        }
-        val backgroundScope = CoroutineScope(Dispatchers.IO)
-        backgroundScope.launch {
-            // Initialize the Google Mobile Ads SDK on a background thread.
-            MobileAds.initialize(this@MainActivity) {}
         }
     }
 }

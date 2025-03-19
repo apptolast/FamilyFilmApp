ebc753f82f78b04592951386310517c0b8ac5963
Renendo <rndeveloper11501@gmail.com>
2025-03-19 19:54:47 +0100

    FFA-166 Shrink resources on release builds.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,7 @@ android {
 
         getByName("release") {
             isMinifyEnabled = true
-
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
